c651cd22e5984db153ad668b589d81c7f847a2da
bettyrjc <bettyjimenez3010@gmail.com>
2019-05-02 11:08:50 -0400

    ApiFriends

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+(async function cargar(){
+
+  async function getdata(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  function template(results){
+    return(
+      `
+      <div class="logo class">
+      <img src="${results.picture.thumbnail}" alt="">
+      <p id="p">${results.name.first} ${results.name.last}</p>
+      </div>
+      `
+    )
+  }
+
+const randomUsername = await getdata('https://randomuser.me/api/?results=10');
+const $itemPlay = document.querySelector('.items')
+  randomUsername.results.forEach( (result)=>{
+    const HTMLString = template(result);
+    const html =document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    $itemPlay.append(html.body.children[0])
+})
+console.log(randomUsername);
+})()
+
+
+
+
 
 const menu =  document.querySelector('.weirdTwo');
 const menuButton =  document.querySelector('#burgerMenu');
